c259ad0d211bf35f631955bacfc2cf7b003a53d8
toster <tostercx@gmail.com>
2018-06-26 15:57:25 +0300

    Add testcase for HasOne relation

--- tests/Models/Vehicle.php
+++ tests/Models/Vehicle.php
@@ -9,10 +9,15 @@ class Vehicle extends Model
     use HasFillableRelations;
     public $timestamps = false;
     protected $fillable = ['name'];
-    protected $fillable_relations = ['wheels'];
+    protected $fillable_relations = ['wheels', 'engine'];
 
     function wheels()
     {
         return $this->hasMany(Wheel::class);
+    }
+
+    function engine()
+    {
+        return $this->hasOne(Engine::class);
     }
 }
